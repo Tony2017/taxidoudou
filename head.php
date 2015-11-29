dfb53869886124e819337c9def8f76484aa02aa0
Dardanboy <sleepersdardan@gmail.com>
2015-11-29 14:51:57 +0100

    Link vers bootstrap.css

--- head.php
+++ head.php
@@ -1,4 +1,5 @@
 <head>
     <meta charset="utf-8" />
     <title>TaxiDoudou</title>
+    <link rel="stylesheet" href="css/bootstrap.css" /> <!-- Mettre bootstrap.min.css dans la version finale pour plus de rapidité -->
 </head>
