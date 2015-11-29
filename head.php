e508aa8d19794be625909c44f32eae434f98ac0d
Dardanboy <sleepersdardan@gmail.com>
2015-11-29 15:07:04 +0100

    add link to style.css

--- head.php
+++ head.php
@@ -2,4 +2,5 @@
     <meta charset="utf-8" />
     <title>TaxiDoudou</title>
     <link rel="stylesheet" href="css/bootstrap.css" /> <!-- Mettre bootstrap.min.css dans la version finale pour plus de rapidité -->
+    <link rel="stylesheet" href="css/style.css" />
 </head>
