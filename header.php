72ca868fbd07167c69205cec7503e196c076095b
Dardanboy <sleepersdardan@gmail.com>
2015-11-29 16:44:45 +0100

    Change header + css
    Modified header elements and css

--- header.php
+++ header.php
@@ -1,23 +1,26 @@
 <header>
-   <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">TaxiDoudou</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="#">Accueil</a></li>
-            <li><a href="#">Prestations</a></li>
-            <li><a href="#">Nous trouver</a></li>
-            <li><a href="#">Notre flotte</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+ <nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">TaxiDoudou</a>
+    </div>
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Prestations</a></li>
+        <li><a href="#">Nous trouver</a></li>
+        <li><a href="#">Notre flotte</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">079 846 29 84</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
 </header>
