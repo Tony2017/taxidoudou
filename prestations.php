2709638580a5294d3b42884cab5a8d667bf0a69b
Tony2017 <tony2017@live.fr>
2015-11-30 19:16:44 +0100

    up prestations

--- prestations.php
+++ prestations.php
@@ -1,1 +1,68 @@
-<section id="prestations" class="col-lg-12">prestations</section>
+<section id="prestations" class="col-lg-8 col-lg-offset-2">
+
+<table id="prix" class="table" cellspacing="0" cellpadding="0">
+					<tbody>
+						<tr>
+							<th valign="middle">PRISE EN CHARGE</th>
+							<td valign="middle">6.20</td>
+							<td valign="middle"></td>
+						</tr>
+						<tr>
+							<th valign="middle">TARIF 1 AU KM</th>
+							<td valign="middle">3.-</td>
+							<td valign="middle">de jour à l’intérieur du périmètre</td>
+						</tr>
+						<tr>
+							<th valign="middle">TARIF 2 AU KM</th>
+							<td valign="middle">3.80</td>
+							<td valign="middle">à l’extérieur du périmètre</td>
+						</tr>
+						<tr>
+							<th valign="middle">TARIF 3 AU KM</th>
+							<td valign="middle">3.80</td>
+							<td valign="middle">de nuit 20h-6h, les dimanches et jours fériés
+						</tr>
+						<tr>
+							<th valign="middle">ATTENTE</th>
+							<td valign="middle">54.-</td>
+							<td valign="middle">par heure</td>
+						</tr>
+						<tr>
+							<th valign="middle">BAGAGES</th>
+							<td valign="middle">1.-</td>
+							<td valign="middle">par tranche de 30 kg, franchise 10 kg
+							</td>
+						</tr>
+						<tr>
+							<th valign="middle">SIÈGE ENFANT</th>
+							<td valign="middle">25.-</td>
+							<td valign="middle"></td>
+						</tr>
+						<tr>
+							<th valign="middle">SIÈGE REHAUSSEUR</th>
+							<td valign="middle">5.-</td>
+							<td valign="middle">dès le 2ème siège</td>
+						</tr>
+						<tr>
+							<th valign="middle">POUSSETTE, SKIS</th>
+							<td valign="middle">1.-</td>
+							<td valign="middle"></td>
+						</tr>
+						<tr>
+							<th valign="middle">CHIEN</th>
+							<td valign="middle">1.-</td>
+							<td valign="middle"></td>
+						</tr>
+						<tr>
+							<td colspan="3">
+								Le client peut demander une quittance détaillée de la course.<br />
+								Pour les courses dont le prix dépasse 50.-, le chauffeur peut demander un dépôt.<br />
+								Un supplément peut être demandé si la course s’effectue entièrement à l’extérieur du périmètre.
+								Transports encombrants: selon entente préalable.
+							</td>
+
+						</tr>
+					</tbody>
+				</table>
+
+</section>
