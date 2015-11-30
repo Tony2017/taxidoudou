44bd2a3b1a05a54cff8da8ead491dcbb975f9cfc
Tony2017 <tony2017@live.fr>
2015-11-30 19:59:08 +0100

    split css files

--- head.php
+++ head.php
@@ -3,4 +3,5 @@
     <title>TaxiDoudou</title>
     <link rel="stylesheet" href="css/bootstrap.css" /> <!-- Mettre bootstrap.min.css dans la version finale pour plus de rapidité -->
     <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/font.css" />
 </head>
