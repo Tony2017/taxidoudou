2ab00f86fc51ef5466dd8b6de1bcf5735f735660
Dardanboy <sleepersdardan@gmail.com>
2015-12-01 21:31:23 +0100

    Add block on the #accueil + change lot of css

--- accueil.php
+++ accueil.php
@@ -1,4 +1,9 @@
 <section id="accueil">
-	<h1>Taxi Estavayer</h1>
-	<h2>Taxi sur Estavayer, Taxi de nuit</h2>
+	<div class="col-lg-offset-1 col-lg-7 col-md-offset-0 col-md-6 col-sm-offset-0 col-sm-12 col-xs-offset-0 col-xs-12">
+		<h1>Taxi Estavayer</h1>
+		<h2>Taxi sur Estavayer, Taxi de nuit</h2>
+	</div>
+	<div class="col-lg-offset-0 col-lg-3 col-md-offset-0 col-md-5 col-sm-offset-2 col-sm-8  col-xs-offset-1 col-xs-10 right-seo-block">
+		This will contain informations like: Number, Map ancre and so on (maximum 3 or 4 informations)
+	</div>
 </section>
