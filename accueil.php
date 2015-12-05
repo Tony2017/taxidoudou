c6361a8b1587d3ff5d97fcb6ff7315206d08ba02
Dardanboy <sleepersdardan@gmail.com>
2015-12-05 12:09:25 +0100

    Add boxes in the right seo-block and some css modifications

--- accueil.php
+++ accueil.php
@@ -3,7 +3,18 @@
 		<h1>Taxi Estavayer</h1>
 		<h2>Taxi sur Estavayer, Taxi de nuit</h2>
 	</div>
-	<div class="col-lg-offset-0 col-lg-3 col-md-offset-0 col-md-5 col-sm-offset-2 col-sm-8  col-xs-offset-1 col-xs-10 right-seo-block">
-		This will contain informations like: Number, Map ancre and so on (maximum 3 or 4 informations)
+	<div id="main-box" class="col-lg-offset-0 col-lg-3 col-md-offset-0 col-md-5 col-sm-offset-2 col-sm-8  col-xs-offset-1 col-xs-10">
+		<div class="box box-title">
+			<h3>Taxi à Estavayer-le-Lac</h3>
+		</div>
+		<div class="box box-number">
+    		079 846 29 84
+		</div>
+		<div class="box">
+    		079 846 29 84
+		</div>
+		<div class="box">
+    		079 846 29 84
+		</div>
 	</div>
 </section>
