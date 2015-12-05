3e1ad43c566026cc582d242dda8ea27f1b8bd871
Dardanboy <sleepersdardan@gmail.com>
2015-12-05 14:38:17 +0100

    centered element on header.php (with container and not container-fluid)

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header>
  <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
+  <div class="container">
     <div class="navbar-header">
       
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -11,6 +11,7 @@
       </button>
       <a class="navbar-brand" href="#">TaxiDoudou</a>
     </div>
+    
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
         <li><a href="#">Accueil</a></li>
