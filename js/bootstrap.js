c0e68c9b36321e799a21f7dc2cb863b902fda078
Dardanboy <sleepersdardan@gmail.com>
2016-04-20 20:59:38 +0200

    asd
    asd

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,12 +1,12 @@
 /*!
  * Bootstrap v3.3.5 (http://getbootstrap.com)
- * Copyright 2011-2016 Twitter, Inc.
+ * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
 /*!
- * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=0cbbd727dc6be2577499)
- * Config saved to config.json and https://gist.github.com/0cbbd727dc6be2577499
+ * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=8587376e49cc39f722c3)
+ * Config saved to config.json and https://gist.github.com/8587376e49cc39f722c3
  */
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
