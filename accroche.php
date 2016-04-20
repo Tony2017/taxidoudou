c4a3db7fbde354e5dd1c88d978bc2a73668e993f
Dardanboy <sleepersdardan@gmail.com>
2016-04-20 21:08:42 +0200

    Revert "Important commit before important changements"
    This reverts commit 1ff9643c5ab6d883218c5c9fcd3fcb5997a1ea66.
    
    # Conflicts:
    #	accroche.php
    #	accueil.php
    #	css/style.css
    #	header.php

--- accroche.php
+++ accroche.php
@@ -1,35 +1,35 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 <section id="services">
 	<div class="container">
 		<div class="row">
-			<section class="col-md-4">
+			<section class="col-md-3">
 				<div class="box-services">
-					<figure class="box-img"><!--<img src="css/img/24_24.png" alt="Taxi 24/24 non-stop"></img>--><img src="css/img/mercedes-cropped.png" alt="Mercedes" height="120px" style="opacity: 0.75; filter: alpha(opacity=75);" /></figure>
-					<!--<h3>24h/24</h3>-->
-
+					<figure class="box-img"><img src="css/img/24_24.png" alt="Taxi 24/24 non-stop"></img></figure>
+					<h3>24h/24</h3>
 				</div>
 			</section>
-			<section class="col-md-4">
+			<section class="col-md-3">
 				<div class="box-services">
-					<figure class="box-img"><!--<img src="css/img/taxi_luxe.png" alt="Taxi de luxe"></img>--><img src="css/img/volkswagen-cropped.png" alt="Mercedes" height="120px" style="opacity: 0.75; filter: alpha(opacity=75);"/></figure>
+					<figure class="box-img"><img src="css/img/taxi_luxe.png" alt="Taxi de luxe"></img></figure>
 					<h3>Taxis de gamme</h3>
 				</div>
 			</section>
-			<section class="col-md-4">
+			<section class="col-md-3">
 				<div class="box-services">
-					<figure class="box-img"><!--<img src="css/img/bus.png" alt="Remorquee"></img>--><img src="css/img/mercedes2-cropped.png" alt="Mercedes" height="90px" style="opacity: 0.75; filter: alpha(opacity=75);"/></figure>
+					<figure class="box-img"><img src="css/img/bus.png" alt="Remorquee"></img></figure>
 					<h3>Fêtes/Groupes</h3>
 				</div>
 			</section>
-			
+			<section class="col-md-3">
+				<div class="box-services">
+					<figure class="box-img"><img src="css/img/remorque.png" alt="Remorquee"></img></figure>
+					<h3>Remorque</h3>
+				</div>
+			</section>
 	</div>
 </section>
->>>>>>> parent of 2271271... Last ones
-=======
->>>>>>> parent of 1ff9643... Important commit before important changements
 <section id="accroche">		
+	<div "col-md-4">
+	</div>
 	<h2>Service 24H/24H</h2>
 	<h3>079 846 29 84</h3>
 </section>
--- header.php
+++ header.php
@@ -9,48 +9,20 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <a class="navbar-brand" href="#">
-        <img src="css/img/TaxiDoudou.png" alt="TaxiDoudou" style="margin-top: -7px; max-width:100px;" />
-      </a>
-=======
       <!--<a class="navbar-brand" href="#">
         <img src="css/img/TaxiDoudou.png" alt="TaxiDoudou" style="margin-top: -7px; max-width:100px;" />
       </a> -->
->>>>>>> parent of 2271271... Last ones
-=======
-      <a class="navbar-brand" href="#">
-        <img src="css/img/TaxiDoudou.png" alt="TaxiDoudou" style="margin-top: -7px; max-width:100px;" />
-      </a>
->>>>>>> parent of 1ff9643... Important commit before important changements
     </div>
     
     <div id="navbar" class="navbar-collapse collapse">
       <ul class="nav navbar-nav">
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <li><a href="#">Accueil</a></li>
-=======
         <li class="active"><a href="#">Accueil</a></li>
->>>>>>> parent of 2271271... Last ones
-=======
-        <li><a href="#">Accueil</a></li>
->>>>>>> parent of 1ff9643... Important commit before important changements
         <li><a href="#">Prestations</a></li>
         <li><a href="#">Nous trouver</a></li>
         <li><a href="#">Notre flotte</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right hidden-sm">
-<<<<<<< HEAD
-<<<<<<< HEAD
-        <li><a href="#"><img src="css/img/phone.png" alt="Phone" class="top-right-navbar-phone"></img>079 846 29 84</a></li>
-=======
         <li><img src="css/img/phone.png" alt="Phone" class="top-right-navbar-phone"><a href="#" style="display: inline-block;"></img>079 846 29 84</a></li>
->>>>>>> parent of 2271271... Last ones
-=======
-        <li><a href="#"><img src="css/img/phone.png" alt="Phone" class="top-right-navbar-phone"></img>079 846 29 84</a></li>
->>>>>>> parent of 1ff9643... Important commit before important changements
       </ul>
 
     </div>
